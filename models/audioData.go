fce57245da390cf2c71142e52d642c4e0b554446
Michael Rodriguez <rodriguez.e.mike@gmail.com>
2025-01-06 23:14:09 -0800

    First commit for streaming audio

--- models/audioData.go
+++ models/audioData.go
@@ -8,6 +8,37 @@ import "github.com/gopxl/beep"
 //This file just holds the idea for now.
 //But should have something like addToBUffer or addToSequence
 //etc.
-type AudioData struct {
-	streams []beep.StreamSeekCloser
+type AudioStreamQueue struct {
+	streamers []beep.Streamer
+}
+
+func (streamQueue *AudioStreamQueue) Err() error {
+	return nil
+}
+
+func (streamQueue *AudioStreamQueue) Add(streamers ...beep.Streamer) {
+	streamQueue.streamers = append(streamQueue.streamers, streamers...)
+}
+
+func (streamQueue *AudioStreamQueue) PopStream() {
+	streamQueue.streamers = streamQueue.streamers[1:]
+}
+
+func (streamQueue *AudioStreamQueue) stream(samples [][2]float64) (n int, ok bool) {
+	streamed := 0
+	for streamed < len(samples) {
+		if len(streamQueue.streamers) == 0 {
+			for i := range samples[streamed:] {
+				samples[i][0] = 0
+				samples[i][0] = 0
+			}
+			break
+		}
+		n, ok := streamQueue.streamers[0].Stream(samples[streamed:])
+		if !ok {
+			streamQueue.PopStream()
+		}
+		streamed += n
+		return len(samples), true
+	}
 }
